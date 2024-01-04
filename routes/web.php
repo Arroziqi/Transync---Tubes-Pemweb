4d66fcb6b31671e69b123d50d133fcdb2813b0e7
Arroziqi <ahmadarroziqi@gmail.com>
2024-01-04 10:49:45 +0700

    fe signin signup home

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+Route::get('/team', function () {
+    return view('team');
+});
+Route::get('/signIn', function () {
+    return view('signIn');
+});
+Route::get('/signUp', function () {
+    return view('signUp');
 });
